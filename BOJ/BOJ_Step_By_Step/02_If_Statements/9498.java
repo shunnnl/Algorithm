dce170b5627131e474d2bd272f3ee5982692ffcf
shunnnl <siyun2072@naver.com>
2025-05-30 22:12:48 +0900

    [Bronze V] Title: 시험 성적, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

---
+++ BOJ/BOJ_Step_By_Step/02_If_Statements/9498.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+
+        if(n>=90)System.out.println("A");
+        else if(n>=80)System.out.println("B");
+        else if(n>=70)System.out.println("C");
+        else if(n>=60)System.out.println("D");
+        else System.out.println("F");
+    }
+}
